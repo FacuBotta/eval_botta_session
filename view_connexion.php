da6a956d16f6bcea6f782dbe5d2e73582df70e91
FacuBotta <nuestampados@gmail.com>
2022-11-24 16:04:26 +0100

    modifications html

---
+++ view_connexion.php
@@ -0,0 +1,16 @@
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Connexion :</title>
+</head>
+<body>
+	<h1>Connexion : </h1>
+	<from action="" method="post">
+		<p>Veuillez saisir votre identifiant :</p>
+		<input type="text" name="login">
+		<p>Veuillez saisir votre mot de passe :</p>
+		<input type="text" name="mdp">
+		<input type="submit" value="connexion">
+	</from>
+</body>
+</html>
